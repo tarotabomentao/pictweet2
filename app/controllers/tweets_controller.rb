9d691d2eb08e97f20c1dbc3cfd51f642d56b3391
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-07-31 17:09:11 +0900

    createアクションの定義、create.html.erbの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,16 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :text, :image)
   end
 
 end
