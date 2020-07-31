d608f74450ffbffcabe77d11022ae62557ec6d03
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-07-31 18:57:25 +0900

    updateのルーティング、アクションの定義、update.html.erb作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]
   root to: 'tweets#index'
 end
