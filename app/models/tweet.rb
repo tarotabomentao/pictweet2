b65e7dd8682f834761574a2960c3bd6fc0f4d668
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-02 13:04:33 +0900

    searchメソッド、アクションの定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,12 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   has_many :comments
   belongs_to :user
+
+  def self.search(search)
+    if search
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
