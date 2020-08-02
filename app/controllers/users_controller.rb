cc851719364050b63baf6484ee6a27552f78526f
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-02 09:12:51 +0900

    もろもろ2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
   end
 
 end
